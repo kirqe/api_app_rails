c123ea23aa29f2663a55d9968297eddf1bcd5bbe
railsr <ikll7@yahoo.com>
2016-03-30 04:18:28 +0400

    added user auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+  validates_uniqueness_of :email
+  
+
+  before_create :generate_authenticaion_token!
+
+  def generate_authenticaion_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
+
 end
