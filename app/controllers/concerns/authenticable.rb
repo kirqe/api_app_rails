e4048dc95b21a62210442cf255324b29b90304f6
railsr <ikll7@yahoo.com>
2016-04-17 05:06:37 +0400

    added the authenticate with token method to handle access to actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,4 +3,9 @@ module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers["Authorization"])
   end
+
+  def authenticate_with_token!
+    render json: { errors: "Not autneticated" },
+                   status: :unauthorized unless current_user.present?
+  end
 end
