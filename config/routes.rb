81feafc6fb1c3fce04de42ae4910a7129c5547c0
railsr <ikll7@yahoo.com>
2016-04-29 04:32:11 +0400

    Adds the show action for order

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
       end
       resources :sessions, only: [:create, :destroy]
       resources :products, only: [:show, :index]
-      resources :orders, :only => [:index]
+      resources :orders, :only => [:index, :show]
     end
   end
 end
